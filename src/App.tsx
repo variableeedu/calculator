7e361ff712735b4c486c37ae4966c23f460176a9
sumanchoudhary203 <sc3868181@gmail.com>
2024-02-04 23:07:50 +0700

    now

--- src/App.tsx
+++ src/App.tsx
@@ -21,44 +21,41 @@ function App() {
             <IoBatteryHalf size={21} />
           </div>
         </div>
-        <div className="h-9 flex justify-end p-3 bg-gray-100">
+        <div className="h-9 flex justify-end p-3 bg-gray-200">
           <BsThreeDotsVertical />
         </div>
-        <div className="h-48 bg-green-200  rounded-b-3xl text-[30px] flex justify-end p-12">
-          <div className="">
-            <div>5678</div>
-            <div className="">_</div>
-          </div>
+        <div className="h-48 bg-gray-200   rounded-b-3xl">
+          <input className="h-12  bg-gray-200  w-full text-[30px] text-end flex justify-end p-12 " />
         </div>
-        <div className=" h-[460px] p-2 bg-red-300">
-          <div className="h-12 pl-4 flex space-x-3 bg-pink-300">
-            <div className="h-12 p-2 w-12 rounded-full  bg-white">
-              <LiaSquareRootAltSolid size={30} />
-            </div>
-            <div className="h-12z w-12 rounded-full  bg-white"></div>
-            <div className="h-12z w-12 rounded-full text-[28px] text-center bg-white">
+        <div className=" h-[460px] p-2 ">
+          <div className="h-12 pl-4 flex space-x-3 ">
+            <LiaSquareRootAltSolid
+              size={30}
+              className="h-12 p-2 w-12 rounded-full  bg-blue-50"
+            />
+            <div className="h-12z w-12 rounded-full  bg-blue-50"></div>
+            <div className="h-12z w-12 rounded-full text-[28px] text-center bg-blue-50">
               ^
             </div>
-            <div className="h-12z w-12 rounded-full text-[28px] text-center bg-white">
+            <div className="h-12z w-12 rounded-full text-[28px] text-center bg-blue-50">
               !
             </div>
-            <div className="h-8 w-8 rounded-full text-[20px] text-center rotate-180 bg-white">
+            <div className="h-8 w-8 rounded-full text-[20px] text-center rotate-180 bg-blue-50">
               ^
             </div>
           </div>
           <div className="pt-1 space-y-1 ">
             <div className="   flex space-x-2">
-              <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-white">
+              <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-blue-200">
                 AC
               </div>
-              <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-white">
+              <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-blue-200">
                 ( )
               </div>
-              <div className="h-[74px] rounded-full   text-[30px] pt-3  text-center   w-[74px] bg-white">
+              <div className="h-[74px] rounded-full   text-[30px] pt-3  text-center   w-[74px] bg-blue-200">
                 %
               </div>
-              <div className="h-[74px] p-5 rounded-full  w-[74px] bg-white">
-                {" "}
+              <div className="h-[74px] p-5 rounded-full  w-[74px] bg-blue-200">
                 <RiDivideLine size={35} />
               </div>
             </div>
@@ -72,7 +69,7 @@ function App() {
               <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-white">
                 9
               </div>
-              <div className="h-[74px]  p-5  rounded-full  w-[74px] bg-white">
+              <div className="h-[74px]  p-5  rounded-full  w-[74px] bg-blue-200">
                 <RxCross2 size={33} />
               </div>
             </div>
@@ -86,7 +83,7 @@ function App() {
               <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-white">
                 6
               </div>
-              <div className="h-[74px] rounded-full text-center text-[50px]  w-[74px] bg-white">
+              <div className="h-[74px] rounded-full text-center text-[50px]  w-[74px] bg-blue-200">
                 -
               </div>
             </div>
@@ -100,7 +97,7 @@ function App() {
               <div className="h-[74px] rounded-full  text-[30px] pt-3  text-center    w-[74px] bg-white">
                 3
               </div>
-              <div className="h-[74px] rounded-full  text-[40px] pt-1  text-center    w-[74px] bg-white">
+              <div className="h-[74px] rounded-full  text-[40px] pt-1  text-center    w-[74px] bg-blue-200">
                 +
               </div>
             </div>
@@ -114,7 +111,7 @@ function App() {
               <div className="h-[74px] rounded-full p-5 text-center   w-[74px] bg-white">
                 <FaDeleteLeft size={30} />
               </div>
-              <div className="h-[74px] rounded-full  text-[40px] pt-1  text-center    w-[74px] bg-white">
+              <div className="h-[74px] rounded-full  text-[40px] pt-1  text-center    w-[74px] bg-blue-200">
                 =
               </div>
             </div>
